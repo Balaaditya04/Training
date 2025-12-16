ec0a1739c7699d9509462cbc8a02b0611ec9573b
Balaaditya04 <aditya.taninki@gmail.com>
2025-12-16 11:34:36 +0530

    16th Dec

--- src/threads.java
+++ src/threads.java
@@ -1,2 +1,35 @@
+class ThreadOne implements Runnable {
+
+    @Override
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Thread One: " + i);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                System.out.println(e);
+            }
+        }
+    }
+}
+
 public class threads {
+
+    public static void main(String[] args) {
+
+        ThreadOne t1 = new ThreadOne();
+        Thread thread1 = new Thread(t1);
+        thread1.start();
+
+        for (int i = 1; i <= 100; i++) {
+            System.out.println("Main Thread: " + i);
+            try {
+                Thread.sleep(700);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                System.out.println(e);
+            }
+        }
+    }
 }
